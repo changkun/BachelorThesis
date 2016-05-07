139f967dd3c0d79bba17f781e2a32acbba202b70
Changkun Ou <ouchangkun@outlook.com>
2016-05-07 19:52:56 +0800

    add CODE_SIGNING_REQUIRED=NO

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 cd server && npm install && npm test
 cd ..
-cd client && cd 0-demo-crown-circle && xcodebuild -project demo-crown-circle.xcodeproj
+cd client && cd 0-demo-crown-circle && xcodebuild -project demo-crown-circle.xcodeproj CODE_SIGNING_REQUIRED=NO
 cd ../..
-cd client && cd 1-Demo-Tap && xcodebuild -project 1-Demo-Tap.xcodeproj
+cd client && cd 1-Demo-Tap && xcodebuild -project 1-Demo-Tap.xcodeproj CODE_SIGNING_REQUIRED=NO
 cd ../..
-cd client && cd 5-Demo-Game && xcodebuild -project 5-Demo-Game.xcodeproj
+cd client && cd 5-Demo-Game && xcodebuild -project 5-Demo-Game.xcodeproj CODE_SIGNING_REQUIRED=NO
